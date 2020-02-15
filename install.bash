e3396fc9fa1d0362fee2b832bc28d74777254d68
Henning Häcker <henning.haecker+github.com@gmail.com>
2020-02-15 12:12:52 +0100

    fix bash script (#6)

--- install.bash
+++ install.bash
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 cd ~/src/github.com/hacker-h/dotfiles && git pull origin master || git clone git@github.com:hacker-h/dotfiles.git ~/src/github.com/hacker-h/dotfiles
 find ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.old
