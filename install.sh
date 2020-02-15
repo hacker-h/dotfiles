af3a7da673e39f4fcd9a8137eb25ded2be0e3578
Henning Häcker <henning.haecker+github.com@gmail.com>
2020-02-15 11:14:08 +0100

    make install script immediately take effect (#3)

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@ set -eu
 cd ~/src/github.com/hacker-h/dotfiles && git pull || git clone git@github.com:hacker-h/dotfiles.git ~/src/github.com/hacker-h/dotfiles
 find ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.old
 ln -s ~/src/github.com/hacker-h/dotfiles/.bash_aliases ~/.bash_aliases
+source ~/.bashrc
 echo "Your dotfiles are now up to date!"
