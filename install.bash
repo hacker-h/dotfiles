63b472f31bba3d39508c454948b2f87571f2d860
Henning Häcker <henning.haecker+github.com@gmail.com>
2020-02-20 16:29:04 +0100

    fix dash compatibility (#7)

--- install.bash
+++ install.bash
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 set -eu
 cd ~/src/github.com/hacker-h/dotfiles && git pull origin master || git clone git@github.com:hacker-h/dotfiles.git ~/src/github.com/hacker-h/dotfiles
-find ~/.bash_aliases && mv ~/.bash_aliases ~/.bash_aliases.old
+find ~/.bash_aliases >/dev/null 2>/dev/null && mv ~/.bash_aliases ~/.bash_aliases.old
 ln -s ~/src/github.com/hacker-h/dotfiles/.bash_aliases ~/.bash_aliases
-source ~/.bashrc
+sh ~/.bashrc
 echo "Your dotfiles are now up to date!"
