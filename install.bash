8e224ec1b7773fd758d8d01dd211bf3b4101cdc1
Henning Häcker <henning.haecker+github.com@gmail.com>
2020-07-11 20:13:41 +0200

    fix immediate bashrc update

--- install.bash
+++ install.bash
@@ -3,5 +3,5 @@ set -eu
 cd ~/src/github.com/hacker-h/dotfiles && git pull origin master || git clone git@github.com:hacker-h/dotfiles.git ~/src/github.com/hacker-h/dotfiles
 find ~/.bash_aliases >/dev/null 2>/dev/null && mv ~/.bash_aliases ~/.bash_aliases.old
 ln -s ~/src/github.com/hacker-h/dotfiles/.bash_aliases ~/.bash_aliases
-sh ~/.bashrc
+source ~/.bashrc
 echo "Your dotfiles are now up to date!"
